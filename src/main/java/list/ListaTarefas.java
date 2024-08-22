21969a19878ab293ceaa7f53c514861027789ecb
atila.alcantara <jdo5itds7vxq44ekzxybgpzl2ib2bs6wmzm5e2ykhpymicfvuyla>
2024-08-22 12:48:36 -0300

    ListaTarefas adicionada

---
+++ src/main/java/list/ListaTarefas.java
@@ -0,0 +1,43 @@
+package main.java.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> _listaTarefas;
+
+    public ListaTarefas() {
+        this._listaTarefas = new ArrayList<>();
+    }
+
+    public void AdicionarTarefa(String descricao){
+        _listaTarefas.add(new Tarefa(descricao));
+    }
+
+    public void RemoverTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for(Tarefa t : _listaTarefas){
+            if (t.get_descricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+
+        _listaTarefas.removeAll(tarefasParaRemover);
+    }
+
+    public int ObterNumeroTotalDeTarefas(){
+        return _listaTarefas.size();
+    }
+
+    public List<String> ObterDescricoesTarefas(){
+        List<String> listaDescricoes = new ArrayList<String>();
+
+        for(Tarefa tarefa : _listaTarefas){
+            listaDescricoes.add(tarefa.get_descricao());
+        }
+
+        return listaDescricoes;
+    }
+}
---
+++ src/main/java/list/Tarefa.java
@@ -0,0 +1,15 @@
+package main.java.list;
+
+public class Tarefa {
+    private String _descricao;
+
+    public Tarefa(String _descricao) {
+        this._descricao = _descricao;
+    }
+
+    public String get_descricao() {
+        return _descricao;
+    }
+
+
+}
